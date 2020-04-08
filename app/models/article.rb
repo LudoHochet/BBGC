276b09f06e9b138b26e18b433b505359d229b7e6
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-04-08 08:07:43 +0200

    add validation in model article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ApplicationRecord
+  CATEGORY = ['news', 'résultats', 'strategie', 'article']
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :category, presence: true, inclusion: {in: CATEGORY }
 end
+
+
+
