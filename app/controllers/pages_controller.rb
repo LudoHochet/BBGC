d915860f1c568fc4e6a12c5df693021749bd4cac
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-04-09 16:25:17 +0200

    display the button to update and destroyarticle only for admin

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,7 @@ class PagesController < ApplicationController
   def home
     # @articles = Article.all.order(:created_at)
     @articles = policy_scope(Article).order(created_at: :desc)
+    @article = Article.new
   end
 end
 
