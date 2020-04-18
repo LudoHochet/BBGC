86901b86f2290e258bf041058d61f9ed683cf805
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-04-18 15:21:24 +0200

    add deliver_now in contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,7 @@ skip_before_action :authenticate_user!, only: :create
     @contact = Contact.new(contact_params)
     authorize @contact
     if @contact.save
-      UserMailer.contact_message(@contact).deliver
+      UserMailer.contact_message(@contact).deliver_now
       flash.notice = "Merci pour votre message, je reviens vers vous rapidement."
       redirect_to root_path
     else
