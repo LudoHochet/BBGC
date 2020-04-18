e88530da1a2c2fc5b820c656dd7e4d1286c28d77
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-04-18 07:51:55 +0200

    add route to contact page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/club', to: 'pages#club'
+  get '/contact', to: 'pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles
 end
