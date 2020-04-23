3f42e4b0f8b1934ad1dc2d31127ab9f381e91353
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-04-23 23:51:40 +0200

    create and display the reviews with pundit

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,19 @@
 class ReviewsController < ApplicationController
   def create
+    @review = Review.new(review_params)
+    @article = Article.find(params[:article_id])
+    @review.article = @article
+    @review.user = current_user
+    authorize @review
+    if @review.save
+      redirect_to article_path(@article)
+    else
+      render 'articles/show'
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:content)
   end
 end
