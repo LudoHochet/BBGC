0170858122210962d794e48b8ad7b4f279ffd730
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-04-24 00:07:58 +0200

    add first name and last name in login form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :reviews
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
