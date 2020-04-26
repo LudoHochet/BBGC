5ac1f1ea2b12732914f354fee6718629c2ca66d2
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-04-26 16:15:49 +0200

    fix bug js

--- app/javascript/plugins/typed_js.js
+++ app/javascript/plugins/typed_js.js
@@ -2,17 +2,20 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   const author = document.getElementById('author');
-  new Typed('#citation', {
-    strings: ["Le jeu est la forme la plus élevée de la recherche"],
-    typeSpeed: 60,
-    loop: false,
-    showCursor: false,
-    backDelay: 700,
-    onComplete: function () {
-      $(".typed-cursor").hide();
-      author.innerText = "Albert Einstein";
-    }
-  });
+  const citation = document.getElementById('citation');
+  if (citation) {
+    new Typed('#citation', {
+      strings: ["Le jeu est la forme la plus élevée de la recherche"],
+      typeSpeed: 60,
+      loop: false,
+      showCursor: false,
+      backDelay: 700,
+      onComplete: function () {
+        $(".typed-cursor").hide();
+        author.innerText = "Albert Einstein";
+      }
+    });
+  }
 }
 
 
