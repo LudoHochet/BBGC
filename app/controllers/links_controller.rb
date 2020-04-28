d47464409f0c0810149e4d5e0d1aa5b1fca5a084
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-04-28 14:29:18 +0200

    add form link in show

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,12 @@
 class LinksController < ApplicationController
+
+  def create
+    @link = Link.new(links_params)
+  end
+
+  private
+
+  def links_params
+    params.require(:link).permit(:text, :label, :link)
+  end
 end
