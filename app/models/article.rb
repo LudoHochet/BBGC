e8c67866cf0750c63a4eaff94b9f17a39d0cb111
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-05-01 10:27:18 +0200

    fix bug to destroy an article with links

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
   CATEGORY = ['news', 'résultats', 'stratégie', 'article']
   has_many :paragraphs, dependent: :destroy
   has_many :reviews
-  has_many :links
+  has_many :links, dependent: :destroy
   has_many_attached :photos
   validates :title, presence: true
   validates :content, presence: true
