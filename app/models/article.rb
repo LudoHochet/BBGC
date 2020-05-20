f6e27a28c3ba7cd097cc548abc1701609bd5b2c4
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-05-20 15:26:35 +0200

    add max lenght validates title article

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,7 +4,7 @@ class Article < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :links, dependent: :destroy
   has_many_attached :photos
-  validates :title, presence: true
+  validates :title, presence: true, length: {in: 1..55}
   validates :content, presence: true
   validates :category, presence: true, inclusion: {in: CATEGORY }
 
